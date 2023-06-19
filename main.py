f6f3540a2fb8070875698a0dfffca8f1b6e3322a
cicidoll <ayaseemt@qq.com>
2023-06-19 15:26:05 +0800

    update output

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from InputProcess import InputProcess
 def main() -> None:
     test = InputProcess()
     print(
-        "转换完成，公钥信息如下： \nDer格式Base64编码公钥值：%s \nRaw格式Hex编码公钥值：%s"
+        "\n转换完成，公钥信息如下： \nDer格式Base64编码公钥值：%s \nRaw格式Hex编码公钥值：%s"
         % (
             test.sm2_pubkey_info.pubkey.base64_der,
             test.sm2_pubkey_info.pubkey.hex_raw
