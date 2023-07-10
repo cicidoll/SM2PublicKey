8a206cda00be9fa3b16c9958e7f13fc53084f6e9
cicidoll <ayaseemt@qq.com>
2023-07-10 10:58:19 +0800

    finished ASN1Der.DerObjects

--- ASN1Der.py
+++ ASN1Der.py
@@ -1,6 +1,6 @@
 from enum import Enum
 from abc import abstractclassmethod
-from typing import List
+from typing import List, Tuple
 from MyError import ASN1DerProcessCode, ASN1DerProcessError
 from StringConvert import StringConvert
 
@@ -30,6 +30,7 @@ class DerIBase:
 
     def _calculation_length(self) -> int:
         """ 计算长度 """
+        #TODO 这个方法要拆出来，或者重新做一个给到DerObjects的split_sequence
         first: str = self._value[2:4] # Length字段的第一个字节
         flag: bool = True if StringConvert.hex_convert_bin(first)[0] == "0" else False # 判断第一个字段的位7是否为0
         if flag:
@@ -108,53 +109,59 @@ class ASN1DerObjectFactory:
     """ 创建ASN1.Der对象-工厂 """
 
     @staticmethod
-    def create():
+    def create_der_object(value: str) -> DerIBase:
         """ 创建对应的对象类型 """
+        tag: str = value[:2]
+        object_name: str = ASN1DerObjectTagsEnum(tag).name
+        return ASN1DerObjectsEnum[object_name].value(value)
 
 class DerObjects:
     """ 拆分整值内的多个Der对象 """
 
     def __init__(self, value: str) -> None:
         # 声明类属性
-        self.der_objects_list: list = []
+        self.der_objects_list: List[DerIBase] = []
         # 赋值类属性
         self.der_objects_list = self._process_values(value)
 
     def _process_values(self, value: str) -> List[DerIBase]:
         data_list: List[str] = [value]
-        result_list: list = []
+        result_list: List[DerIBase] = []
         # 开始处理
         while len(data_list) > 0:
             sub = data_list.pop()
-            sub_object: DerIBase = self._create_der_object(sub)
-            if sub_object.tag != ASN1DerObjectTagsEnum.Sequence.value:
+            sub_object: DerIBase = ASN1DerObjectFactory.create_der_object(sub)
+            if sub_object.tag == ASN1DerObjectTagsEnum.Sequence.value:
+                data_list += self._split_sequence(sub_object.value)
+            else:
                 result_list.append(sub)
-            elif sub_object.tag == ASN1DerObjectTagsEnum.Sequence.value:
-                data_list += self._split_sequence(sub_object.value)            
-
-        return [ self._create_der_object(i) for i in result_list]
+        return [ASN1DerObjectFactory.create_der_object(i) for i in result_list]
 
     def _split_sequence(self, value: str) -> List[str]:
         """ 拆分ASN1.Der.Sequence序列类型 """
         result_list: List[str] = []
         index: int = 0
         while True:
-            sub_value_len: int = StringConvert.hex_convert_int(value[index+2:index+4])
-            result_list.append(value[index:index+sub_value_len*2+4])
-            index = index + sub_value_len*2 + 4
-            if index == len(value): break
+            sub_length_len, sub_value_len = self._calculation_length(value[index:])
+            sub_len: int = sub_length_len*2+sub_value_len*2+2 # 整个子序列类型占的位数（字节数*2）
+            result_list.append(
+                value[index:index+sub_len]
+            )
+            index += sub_len
+            if index >= len(value): break
         return result_list
-
-    def _create_der_object(self, value: str) -> DerIBase:
-        """ 创建单个Der对象 """
-        tag: str = value[:2]
-        object_name: str = ASN1DerObjectTagsEnum(tag).name
-        return ASN1DerObjectsEnum[object_name].value(value)
     
-result: List[DerIBase] = DerObjects("3059301306072A8648CE3D020106082A811CCF5501822D034200046A826E79032BA6144FA1EB22F4F2BA3D2B77EBDC54789C1EF15B64FBCFE7A259E771DDD2FE6DDB377B1A9820B394F58AFA2BF02226EC5DAEFACC14B2A22D4E11").der_objects_list
-
-for i in result:
-    print(i.tag)
-    print(i.length)
-    print(i.value)
-    print('-'*30)
+    def _calculation_length(self, value: str) -> Tuple[int, int]:
+        """ 计算长度-返回字节数 """
+        length_len: int # 计算结果
+        first: str = value[2:4] # Length字段的第一个字节
+        flag: bool = True if StringConvert.hex_convert_bin(first)[0] == "0" else False # 判断第一个字段的位7是否为0
+        if flag:
+            value_len: int = StringConvert.hex_convert_int(first)
+            length_len = 1
+        else:
+            num_len: int = StringConvert.bin_convert_int("0" + StringConvert.hex_convert_bin(first)[1:]) # 计算得到Length字段占用字节长度
+            value_len: int = StringConvert.hex_convert_int(value[4: 4+num_len*2])
+            length_len = 1 + num_len
+        # 返回Value字段实际长度
+        return length_len, value_len
--- test/test_ASN1Der.py
+++ test/test_ASN1Der.py
@@ -1,30 +1,36 @@
 import unittest, sys
+from typing import List
 sys.path.append('.')
 
-from ASN1Der import DerIBase, BitString
+from ASN1Der import DerIBase, BitString, DerObjects, Sequence, Oid
 
+# 测试长度127字节的数据 
 data_127 = "3059301306072A8648CE3D020106082A811CCF5501822D034200046A826E79032BA6144FA1EB22F4F2BA3D2B77EBDC54789C1EF15B64FBCFE7A259E771DDD2FE6DDB377B1A9820B394F58AFA2BF02226EC5DAEFACC14B2A22D4E11"
-
 assert_127_result = {
     "tag": "30",
     "length": "59",
     "value": "301306072A8648CE3D020106082A811CCF5501822D034200046A826E79032BA6144FA1EB22F4F2BA3D2B77EBDC54789C1EF15B64FBCFE7A259E771DDD2FE6DDB377B1A9820B394F58AFA2BF02226EC5DAEFACC14B2A22D4E11"
 }
-
+# 测试长度大于128字节的数据 
 data_129 = "0381810047eb995adf9e700dfba73132c15f5c24c2e0bfc624af15660eb86a2eab2bc4971fe3cbdc63a525ecc7b428616636a1311bbfddd0fcbf1794901de55ec7115ec9559feba33e14c799a6cbbaa1460f39d444c4c84b760e205d6da9349ed4d58742eb2426511490b40f065e5288327a9520a0fdf7e57d60dd72689bf57b058f6d1e"
-
 assert_129_result = {
     "tag": "03",
     "length": "8181",
     "value": "0047eb995adf9e700dfba73132c15f5c24c2e0bfc624af15660eb86a2eab2bc4971fe3cbdc63a525ecc7b428616636a1311bbfddd0fcbf1794901de55ec7115ec9559feba33e14c799a6cbbaa1460f39d444c4c84b760e205d6da9349ed4d58742eb2426511490b40f065e5288327a9520a0fdf7e57d60dd72689bf57b058f6d1e"
 }
-
+# 测试BIT STRING类型
 assert_bitstring_value = "03030457df"
 assert_bitstring_value_result: dict = {
     "tag": "03",
     "length": "03",
     "value": "57d0"
 }
+# 测试拆分整段值内的多个Der对象
+assert_all_value = "3059301306072A8648CE3D020106082A811CCF5501822D034200046A826E79032BA6144FA1EB22F4F2BA3D2B77EBDC54789C1EF15B64FBCFE7A259E771DDD2FE6DDB377B1A9820B394F58AFA2BF02226EC5DAEFACC14B2A22D4E11"
+assert_all_value_result: list = [
+    Sequence, Oid, BitString
+]
+
 
 class TestDerIBase(unittest.TestCase):
 
@@ -48,6 +54,16 @@ class TestDerIBase(unittest.TestCase):
         self.assertEqual(test.tag, assert_bitstring_value_result["tag"])
         self.assertEqual(test.length, assert_bitstring_value_result["length"])
         self.assertEqual(test.value, assert_bitstring_value_result["value"])
+
+    def test_all_value(self):
+        """ 测试整值"""
+        result: List[DerIBase] = DerObjects(assert_all_value).der_objects_list
+        true_flag: int = 3
+        flag = 0
+        for i in assert_all_value_result:
+            for j in result:
+                if isinstance(j, i): flag += 1
+        self.assertTrue(flag == true_flag)
 
 if __name__ == "__main__":
     unittest.main()
